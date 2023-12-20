d1d263379d3a150dfb959537ae2dd4288bd5863d
Marius Van Pee <mariusvanpee@hotmail.be>
2023-12-20 15:05:56 +0100

    add all include

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 14:42:17 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/12/20 14:48:56 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:04:13 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 #define PHILO_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
 
 #endif
