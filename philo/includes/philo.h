13caca6f10694e6be64bf6dd8c95254bfd6c7fc4
Marius Van Pee <mariusvanpee@hotmail.be>
2023-12-20 15:42:54 +0100

    add init info

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,18 +6,27 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 14:42:17 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/12/20 15:04:13 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:42:40 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <pthread.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef struct s_info
+{
+	int	number_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times;
+}		t_info;
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,14 +6,43 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 14:41:32 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/12/20 14:48:46 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:42:35 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int main(void)
+static int	init_info(t_info *info, char **args)
 {
-    printf("Test\n");
-    return 0;
+	int	temp;
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		temp = atoi(args[i]);
+		if (!temp || temp < 0)
+			return (printf("Invalid argument: %d.\n", i + 1), 1);
+		i++;
+	}
+	info->number_of_philo = atoi(args[0]);
+	info->time_to_die = atoi(args[1]);
+	info->time_to_eat = atoi(args[2]);
+	info->time_to_sleep = atoi(args[3]);
+	info->number_of_times = -1;
+	if (args[4])
+		info->number_of_times = atoi(args[4]);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_info	info;
+
+	if (ac != 5 && ac != 6)
+		return (printf("Invalid number(s) of argument(s).\n"));
+	if (init_info(&info, av + 1))
+		return (1);
+	printf("Test\n");
+	return (0);
 }
